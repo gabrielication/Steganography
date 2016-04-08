96bda1020665878973445ca6be5cff7b2cc2c2a4
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2016-04-08 16:39:06 +0200

    Initial commit.

---
+++ app/src/main/java/com/example/gabriele/steganography/IntroActivity.java
@@ -0,0 +1,78 @@
+package com.example.gabriele.steganography;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private static final int REQUEST_TAKE_PHOTO = 1; //intent code for opening the camera app
+
+    private String photo_path; //path of the took picture
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "STG_"+timeStamp;
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"Steganography");
+        if(!storageDir.exists()){
+            storageDir.mkdir();
+            Log.i("degab","dir "+storageDir.getAbsolutePath()+" created");
+        }
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".png",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        photo_path = image.getAbsolutePath();
+        return image;
+    }
+
+    public void takePhoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) { //if the user clicked ok, we can pass the path to the Activity
+            Intent selfiSrc = new Intent(this, this.getClass()); //TODO
+            selfiSrc.putExtra("imgurl", photo_path);
+            startActivity(selfiSrc);
+        }
+
+    }
+}
