e4ffc0d94afc5b77575ec24ba5db0d9edc57bb75
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2016-04-08 20:24:04 +0200

    Debug mode added.

---
+++ app/src/main/java/com/example/gabriele/steganography/steganography/DecodeRS.java
@@ -0,0 +1,8 @@
+package com.example.gabriele.steganography.steganography;
+
+/**
+ * Created by gabriele on 08/04/16.
+ */
+public class DecodeRS {
+
+}
---
+++ app/src/main/java/com/example/gabriele/steganography/steganography/java/EncodeJava.java
@@ -0,0 +1,7 @@
+package com.example.gabriele.steganography.steganography.java;
+
+/**
+ * Created by gabriele on 08/04/16.
+ */
+public class EncodeJava {
+}
